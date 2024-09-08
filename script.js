9d475f8851c95487ba6c6e507929006e3575a626
zaparoliisabelli <168565957+zaparoliisabelli@users.noreply.github.com>
2024-09-07 22:30:45 -0300

    projeto final

---
+++ script.js
@@ -0,0 +1,12 @@
+const avança = document.querySelectorall('.btn-proximo');
+
+avança.forEach(button=> 
+button.addeventlistener('click',function)(){
+const: atual=document. querySelector('.ativo');
+const: proximopasso ='passo-'+ this.getatribute('data-proximo');
+
+atual,classList,remove:('ativo');
+document.getElementById(proximopasso).classList.add('ativo');
+
+
+})
